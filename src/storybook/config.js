017aed369b07eb89c4ebf4f239452b69429aec87
Dmitry Bukov <bukov.dmitry@gmail.com>
2017-01-03 20:45:08 +0800

    feat(storybook): Why did you update, bruh?

--- src/storybook/config.js
+++ src/storybook/config.js
@@ -1,5 +1,12 @@
+import React from 'react'
+import { whyDidYouUpdate } from 'why-did-you-update'
 import { configure, addDecorator } from '@kadira/storybook'
 
+// https://github.com/garbles/why-did-you-update
+whyDidYouUpdate(React, {
+	//include: /^pure/,
+	exclude: /^Connect/
+})
 
 // В этой функции подключаются необходимые истории
 // эту работу делает специальный лоадер, который берёт аргументы из консоли
