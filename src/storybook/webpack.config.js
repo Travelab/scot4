bc20526e5bf5a50e8720d16084c20193f9e2a022
Dmitry <menscrem@gmail.com>
2017-03-09 17:18:17 +0300

    refactor(storybook): remove dead code

--- src/storybook/webpack.config.js
+++ src/storybook/webpack.config.js
@@ -1,12 +1,5 @@
 import createSCPreset from './loader-presets/select-components'
-import {
- packagesPath,
- entryStorybullPath
-} from '../path'
-
-import css from '../webpack-blocks/css'
-import svg from '../webpack-blocks/svg'
-import image from '../webpack-blocks/image'
+import { entryStorybullPath } from '../path'
 
 // Export a function. Accept the base config as the only param.
 export default function (storybookBaseConfig, configType) {
@@ -14,26 +7,16 @@ export default function (storybookBaseConfig, configType) {
 	// You can change the configuration based on that.
 	// 'PRODUCTION' is used when building the static version of storybook.
 
-	//console.log(storybookBaseConfig.entry.preview)
-	//console.log(storybookBaseConfig.resolveLoader)
-
-
 	storybookBaseConfig.resolve = {
 		...storybookBaseConfig.resolve,
-		modules: [
-			...storybookBaseConfig.resolve.modules,
-			'packages'
-		],
 		alias: {
-			'entry-storybull': entryStorybullPath,
+			...storybookBaseConfig.resolve.alias,
+			'entry-storybull': entryStorybullPath
 		}
 	}
 
 	storybookBaseConfig.module.rules = [
 		...storybookBaseConfig.module.rules,
-		...css({ include: packagesPath })().module.rules,
-		...svg({ include: packagesPath })().module.rules,
-		...image({ include: packagesPath })().module.rules,
 		...createSCPreset(),
 	]
 
