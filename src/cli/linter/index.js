82487941cd1b04d96ed8a059061b671061d9ed09
Dmitry <menscrem@gmail.com>
2017-05-31 12:07:37 +0300

    chore(fix): add roadmap and fix error

--- src/cli/linter/index.js
+++ src/cli/linter/index.js
@@ -1,12 +1,12 @@
 import yo from '../../yo-yo'
 import Generator from './generator.js'
 
-export default (componentName) => {
-	yo([
-		{
-			name: 'linter',
-			generator: Generator,
-			options: { componentName }
-		}
-	])
-
+export default componentName => {
+  yo([
+    {
+      name: 'linter',
+      generator: Generator,
+      options: { componentName }
+    }
+  ])
+}
