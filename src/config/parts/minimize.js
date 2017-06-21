625cecbe5d856d90069255c6a8bbf2a3277acdb2
mikak94 <mikak94@users.noreply.github.com>
2017-06-21 11:59:18 +0300

    feat(sourcemaps): add sourcemaps in build (#72)

--- src/config/parts/minimize.js
+++ src/config/parts/minimize.js
@@ -20,7 +20,7 @@ export default () =>
             dead_code: true,
             drop_console: true
           },
-          sourceMap: false
+          sourceMap: true
         }),
         new ArchivePlugin({
           output: `${buildPath}/build`,
